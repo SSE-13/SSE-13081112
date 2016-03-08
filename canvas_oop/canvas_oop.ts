31bf29a1279692e7f0872a0d63018e4fc20ae983
lifan740973870 <740973870@qq.com>
2016-03-08 23:53:37 +0800

    1

--- canvas_oop/canvas_oop.ts
+++ canvas_oop/canvas_oop.ts
@@ -104,18 +104,18 @@ var context = canvas.getContext("2d");
 
 
 var rect = new Rect();
-rect.width = 550;
+rect.width = 800;
 rect.height = 363;
 rect.color = '#00FFFF'
 
 
-var rect2 = new Rect();
-rect2.width = 300;
-rect2.height = 50;
-rect2.x = 200;
-rect2.y = 200;
-rect2.rotation = Math.PI / 8;
-rect2.color = '#00FFFF'
+//var rect2 = new Rect();
+//rect2.width = 300;
+//rect2.height = 50;
+//rect2.x = 200;
+//rect2.y = 200;
+//rect2.rotation = Math.PI / 8;
+//rect2.color = '#00FFFF'
 
 var text = new TextField();
 text.x = 10;
@@ -126,7 +126,7 @@ bitmap.y = 5;
 bitmap.source = 'tree.png';
 
 //渲染队列
-var renderQueue = [rect, rect2, text,bitmap];
+var renderQueue = [rect, text,bitmap];
 //资源加载列表
 var imageList = ['tree.png'];
 
