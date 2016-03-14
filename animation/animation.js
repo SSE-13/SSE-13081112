2964aea796f628880dac05a082717e16ca1713d6
lifan740973870 <740973870@qq.com>
2016-03-14 21:44:23 +0800

    Finish homework 3

--- animation/animation.js
+++ animation/animation.js
@@ -1,139 +1,98 @@
 /**
  * 重力加速度
  */
-const GRAVITY = 9.8;
-
-const BOUNCE_UP = 0;
-
-const BOUNDS_BOTTOM = 400;
-
-const BOUNDS_LEFT = 0;
-
-const BOUNDS_RIGHT = 400;
-
-const BOUNCE = 0.95;
-
+var GRAVITY = 9.8;
+var BOUNCE_UP = 0;
+var BOUNDS_BOTTOM = 400;
+var BOUNDS_LEFT = 0;
+var BOUNDS_RIGHT = 400;
+var BOUNCE = 0.95;
 //const f = 0.4;
-
 /**
  * 计时器系统
  */
-class Ticker {
-
-    bodyQueue = [];
-
-    lastTime;
-
+var Ticker = (function () {
+    function Ticker() {
+        this.bodyQueue = [];
+    }
     /**
      * 启动计时器
      * @param bodyList 物理队列
      */
-    start(bodyQueue) {
+    Ticker.prototype.start = function (bodyQueue) {
         this.bodyQueue = bodyQueue;
         this.lastTime = Date.now();
         var self = this;
         setInterval(this.onTicker.bind(this), 1000 / 60);
-    }
-
-    onTicker() {
+    };
+    Ticker.prototype.onTicker = function () {
         var currentTime = Date.now();
         var duringTime = currentTime - this.lastTime;
         this.lastTime = currentTime;
-        this.bodyQueue.map(function(body) {
-            body.onTicker(duringTime / 100)
+        this.bodyQueue.map(function (body) {
+            body.onTicker(duringTime / 100);
         });
-    }
-}
-
-
-class Body {
-
-    vx = 0;
-    vy = 0;
-    x = 0;
-    y = 0;
-    width = 0;
-    height = 0;
-    
-
-    displayObject;
-
-    constructor(displayObject: DisplayObject) {
+    };
+    return Ticker;
+}());
+var Body = (function () {
+    function Body(displayObject) {
+        this.vx = 0;
+        this.vy = 0;
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
         this.displayObject = displayObject;
     }
-
-    public onTicker(duringTime) {
-        if(this.vy > -1 && this.vy < 1 && this.y + this.height > BOUNDS_BOTTOM ){
-            this.vy=0;
-            this.vx -= this.vx * 0.1; 
-            if(this.vx < 0.1) {
+    Body.prototype.onTicker = function (duringTime) {
+        if (this.vy > -1 && this.vy < 1 && this.y + this.height > BOUNDS_BOTTOM && this.x != 0) {
+            this.vy = 0;
+            this.vx -= this.vx * 0.1;
+            if (this.vx < 0.1) {
                 this.vx = 0;
             }
             console.log(this.vx);
-            
         }
-        else 
-
-        this.vy += duringTime * GRAVITY;
-      
+        else
+            this.vy += duringTime * GRAVITY;
         this.y += duringTime * this.vy;
-        
         this.x += duringTime * this.vx;
-
-
         //console.log(this.vy);
-
         //反弹
         if (this.y + this.height > BOUNDS_BOTTOM && this.vy > 0) {
             this.vy = -BOUNCE * this.vy;
         }
-        
-        if(this.y < BOUNCE_UP ){
+        if (this.y < BOUNCE_UP) {
             this.vy = -BOUNCE * this.vy;
         }
-        
         if (this.x + this.width > BOUNDS_RIGHT) {
             this.vx = -BOUNCE * this.vx;
         }
-        
         if (this.x < BOUNDS_LEFT) {
-            this.vx = -BOUNCE*this.vx;
+            this.vx = -BOUNCE * this.vx;
         }
-        
-
         //TODO： 左右越界反弹
-
-
-
         //根据物体位置更新显示对象属性
         var displayObject = this.displayObject;
         displayObject.x = this.x;
         displayObject.y = this.y;
-
-    }
-}
-
-
+    };
+    return Body;
+}());
 var rect = new Rect();
 rect.width = 150;
 rect.height = 100;
 rect.color = '#FF0000';
-
 /**
  * 创建一个物体，其显示内容为一个长方形，受重力做平抛运动
  */
 var body = new Body(rect);
 body.width = rect.width;
 body.height = rect.height;
-body.vx =5;//需要保证 vx 在 0-50的范围内行为正常
-body.vy = 0;//需要保证 vy 在 0-50的范围内行为正常
-
-
-
+body.vx = 5; //需要保证 vx 在 0-50的范围内行为正常
+body.vy = 0; //需要保证 vy 在 0-50的范围内行为正常
 var renderCore = new RenderCore();
 var ticker = new Ticker();
-
 renderCore.start([rect]);
 ticker.start([body]);
-
-
